fb5d5245a072c76c1bfc50a9c597c822aa59d0fb
Ubuntu <ubuntu@ip-172-31-32-149.eu-west-1.compute.internal>
2024-02-07 15:12:42 +0000

    adjusting number of dbs

--- k8s/manifests/apply.sh
+++ k8s/manifests/apply.sh
@@ -1,2 +1,5 @@
-for file in $(ls *-deployment.yaml); do kubectl apply -f $file; done
+kubectl apply -f clusterissuer.yaml
+kubectl apply -f db-deployment.yaml
+for file in $(ls *service-deployment.yaml); do kubectl apply -f $file; done
+kubectl apply -f api-gateway-deployment.yaml
 kubectl apply -f api-ingress.yaml
